71f16f9d80fcd7d3dd611818eb9d787f66a30c46
pratiksaxena <pratik2709@gmail.com>
2018-08-15 19:42:17 +0530

    added service worker file

--- main.js
+++ main.js
@@ -100,5 +100,17 @@ var pomodoro = {
     }
 };
 window.onload = function () {
+    installServiceWorkerAsync();
     pomodoro.init();
 };
+
+async function installServiceWorkerAsync() {
+    if ('serviceWorker' in navigator) {
+        try {
+            let serviceWorker = await navigator.serviceWorker.register('sw.js')
+            console.log(`Service worker registered ${serviceWorker}`)
+        } catch (err) {
+            console.error(`Failed to register service worker: ${err}`)
+        }
+    }
+}
