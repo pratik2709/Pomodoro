e1dfb032f1d1c1713b8ba33ba8a168082487fce4
pratiksaxena <pratik2709@gmail.com>
2018-08-15 20:28:01 +0530

    intialized service worker

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE_NAME = "V1"
+
+/**
+ * The install event is fired when the registration succeeds.
+ * After the install step, the browser tries to activate the service worker.
+ * Generally, we cache static resources that allow the website to run offline
+ */
+this.addEventListener('install', async function() {
+    const cache = await caches.open(CACHE_NAME);
+    cache.addAll([
+        'index.html',
+        'css/style.css',
+        'css/circle.css',
+        'main.js',
+    ])
+})
+
